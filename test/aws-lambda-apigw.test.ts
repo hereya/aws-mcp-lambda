4501958e54c87b64becb0f893b1793d2e526c852
Jonatan Sombié <jonatan.sombie@gmail.com>
2026-03-12 17:15:21 +0100

    Initial commit

---
+++ test/aws-lambda-apigw.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLambdaApigw from '../lib/aws-lambda-apigw-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-lambda-apigw-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLambdaApigw.AwsLambdaApigwStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
